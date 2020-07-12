59fad3f6dde5dc3e59b88bbfb6e3878486f49fd1
Matthew Bahler <mattbahler@gmail.com>
2020-07-11 23:30:55 -0600

    better levels n such

--- game/main.js
+++ game/main.js
@@ -375,18 +375,18 @@ function changemood(state, mood)
 	});
 }
 
-function reset_level(scene, ux_scene)
+function reset_level(scene)
 {
 	scene.restart();
-	ux_scene.restart();
 }
 
 document.addEventListener('DOMContentLoaded', function()
 {
 	const dom_container = document.getElementById('container');
 
+	let current_level = 0;
+	let current_level_name;
 	const state = {};
-	let ux_scene_obj;
 
 	const game_scene = new Phaser.Class({
 		Extends: Phaser.Scene,
@@ -426,7 +426,8 @@ document.addEventListener('DOMContentLoaded', function()
 
 			this.lights.enable().setAmbientColor(0x303840);
 
-			const level = this.cache.json.get('level0');
+			const level = this.cache.json.get(`level${current_level}`);
+			current_level_name = level.name;
 
 			state.graph = makegraph(level, tilegroups, state);
 			const node_spawn = state.graph[level.spawn[0]][level.spawn[1]];
@@ -620,16 +621,46 @@ document.addEventListener('DOMContentLoaded', function()
 					sprite.setPipeline('Light2D');
 				}
 			}
+
+			// UX Scene
+			state.ux_stored_light = {
+				sprites: [
+					this.add.sprite(164, 10, 'atlas', 'sconce_unlit'),
+					this.add.sprite(185, 10, 'atlas', 'sconce_unlit'),
+					this.add.sprite(206, 10, 'atlas', 'sconce_unlit')
+				],
+				available: 0
+			};
+
+			const level_text = this.add.text(this.game.canvas.width/2, this.game.canvas.height/2, current_level_name, {fontFamily: 'nightie', fontSize: '27px', backgroundColor: 0x303030, fixedWidth: this.game.canvas.width, fixedHeight: 32, align: 'center'});
+			level_text.setOrigin(0.5, 0.5);
+			level_text.font = 'nightie';
+
+			this.tweens.addCounter({
+				from: 1,
+				to: 0,
+				duration: 2000,
+				onUpdate: function(tween)
+				{
+					level_text.setAlpha(tween.getValue());
+				}
+			});
 		},
 
 		update: function()
 		{
+			const scene = this.scene;
 			if(state.node_current.win)
 			{
 				if(!state.win)
 				{
 					state.win = true;
-					this.add.text(0, 24, 'You Win POGGERS', {fontFamily: 'nightie', fontSize: '27px', fixedWidth: this.game.canvas.width, fixedHeight: 32, align: 'center'}).setOrigin(0, 0);
+					this.add.text(0, 24, 'Sir Daft is Victorious', {fontFamily: 'nightie', fontSize: '27px', fixedWidth: this.game.canvas.width, fixedHeight: 32, align: 'center'}).setOrigin(0, 0);
+					setTimeout(function()
+					{
+						current_level++;
+						reset_level(scene);
+					}, 2000);
 				}
 				return;
 			}
@@ -637,13 +668,12 @@ document.addEventListener('DOMContentLoaded', function()
 			{
 				if(!state.lose)
 				{
-					const scene = this.scene;
 					state.lose = true;
 					state.dummy.anims.play('dummy_fall');
-					this.add.text(0, 24, 'You Died Idiot kekw', {fontFamily: 'nightie', fontSize: '27px', fixedWidth: this.game.canvas.width, fixedHeight: 32, align: 'center'}).setOrigin(0, 0);
+					this.add.text(0, 24, 'Sir Daft has Perished', {fontFamily: 'nightie', fontSize: '27px', fixedWidth: this.game.canvas.width, fixedHeight: 32, align: 'center'}).setOrigin(0, 0);
 					setTimeout(function()
 					{
-						reset_level(scene, ux_scene_obj);
+						reset_level(scene);
 					}, 2000);
 				}
 				return;
@@ -739,44 +769,6 @@ document.addEventListener('DOMContentLoaded', function()
 		}
 	});
 
-	const ux_scene = new Phaser.Class({
-		Extends: Phaser.Scene,
-		initialize: function()
-		{
-			Phaser.Scene.call(this, {key: 'ux_scene', active: true});
-		},
-		preload: function()
-		{
-			this.load.atlas('atlas', ['assets/tileset.png', 'assets/normal.png'], 'assets/tileset.json');
-		},
-		create: function()
-		{
-			ux_scene_obj = this.scene;
-			state.ux_stored_light = {
-				sprites: [
-					this.add.sprite(164, 10, 'atlas', 'sconce_unlit'),
-					this.add.sprite(185, 10, 'atlas', 'sconce_unlit'),
-					this.add.sprite(206, 10, 'atlas', 'sconce_unlit')
-				],
-				available: 0
-			};
-
-			const level_text = this.add.text(this.game.canvas.width/2, this.game.canvas.height/2, 'Upper Dungeon - 1', {fontFamily: 'nightie', fontSize: '27px', backgroundColor: 0x303030, fixedWidth: this.game.canvas.width, fixedHeight: 32, align: 'center'});
-			level_text.setOrigin(0.5, 0.5);
-			level_text.font = 'nightie';
-
-			this.tweens.addCounter({
-				from: 1,
-				to: 0,
-				duration: 2000,
-				onUpdate: function(tween)
-				{
-					level_text.setAlpha(tween.getValue());
-				}
-			});
-		}
-	});
-
 	const game = new Phaser.Game({
 		type: Phaser.AUTO,
 		title: 'GMTK 2020',
@@ -792,7 +784,7 @@ document.addEventListener('DOMContentLoaded', function()
 				fps: 30
 			}
 		},
-		scene: [game_scene, ux_scene]
+		scene: game_scene
 	});
 
 	function resize()
